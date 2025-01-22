10f3c79bfcdbc78be0f17196a14dfb7a96f2f8ad
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-22 10:04:10 +0100

    verschachtelte forschleife

--- exercises/forschleife_übung.java
+++ exercises/forschleife_übung.java
@@ -6,6 +6,12 @@ public class forschleife_übung {
         for(int i = 0;i <= 10; i++){
             System.out.println(i);
         }
+
+        for(int j = 1; j <= 10; j++) {
+            if(j == 5) {
+                System.out.println("Hi, du hast mich gefunden!! " + j);
+            }
+        }
         
     }
 }
