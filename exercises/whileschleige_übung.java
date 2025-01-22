af2ff3b4288772da69d92674bc93d1a6b1d38221
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-22 10:38:19 +0100

    whileschleife_übung.java hinzugefügt und erste whileschleife erstellt

---
+++ exercises/whileschleige_übung.java
@@ -0,0 +1,10 @@
+public class whileschleige_übung {
+
+    public static void main(String[] args) {
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Ich bin: " + count);
+            count++;
+        }
+    }
+}
