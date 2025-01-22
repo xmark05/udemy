4b26c2585343f1b52901e0db089be6dda192c098
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-22 09:59:12 +0100

    erste forschleife erstellt

---
+++ exercises/forschleife_übung.java
@@ -0,0 +1,11 @@
+public class forschleife_übung {
+
+    public static void main(String[] args) {
+        
+
+        for(int i = 0;i <= 10; i++){
+            System.out.println(i);
+        }
+        
+    }
+}
