07aa018f83c453bbb099ebedd94005f61bae4b3c
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-23 10:02:24 +0100

    methode summe hinzugefügt und locale variablen erstellt

---
+++ exercises/methodenreturn2.java
@@ -0,0 +1,17 @@
+public class methodenreturn2 {
+
+    public static void main(String[] args) {
+        
+
+        summe();
+    }
+
+    public static void summe() {
+        int a, b, summe; //local
+        a = 5;
+        b = 5;
+        summe = a + b;
+
+
+    }
+}
