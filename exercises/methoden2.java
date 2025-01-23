14b4e7456bf9c37413a0c7e909d3ea75b4e78552
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-23 07:50:55 +0100

    methode gegnererstellt hinzugefügt und mit schleife wiedergegeben

---
+++ exercises/methoden2.java
@@ -0,0 +1,18 @@
+public class methoden2 {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=10; i++ ) {
+            gegnerErstellen();
+        }
+    }
+
+    static void gegnerErstellen() { 
+
+        String gegnerName = "Mark";
+        int gegnerLeben = 3;
+        System.out.println("Gegner wurde erstellt");
+    }
+
+
+}
