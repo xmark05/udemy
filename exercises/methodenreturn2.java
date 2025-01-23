c9dec64cef2954badc58567183b8d7fc4bb8a866
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-23 10:49:35 +0100

    int summe hinzugefügt

--- exercises/methodenreturn2.java
+++ exercises/methodenreturn2.java
@@ -5,10 +5,13 @@ public class methodenreturn2 {
 
         int a = summe();
         System.out.println("Der Wert aus der Methode ist: " + a);
+
+        int summe = a - 5;
+        System.out.println("Der Wert in summe ist: " + summe);
     }
 
     public static int summe() {
-        int a, b, summe; //local
+        int a, b, summe; //local Variable
         a = 5;
         b = 5;
         summe = a + b;
