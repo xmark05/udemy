42e5909b6241ef687229553a7caa4ed3425ae7a6
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-23 08:05:54 +0100

    Parameter hinzugefügt

--- exercises/methoden2.java
+++ exercises/methoden2.java
@@ -2,17 +2,28 @@ public class methoden2 {
 
     public static void main(String[] args) {
 
-        for(int i = 1; i<=10; i++ ) {
+/*     for(int i = 1; i<=10; i++ ) {
             gegnerErstellen();
         }
+*/
+        erstelleGegner("Peter", 10);
+        erstelleGegner("Olaf", 200);
     }
 
-    static void gegnerErstellen() { 
+ /*    static void gegnerErstellen() { 
 
         String gegnerName = "Mark";
         int gegnerLeben = 3;
         System.out.println("Gegner wurde erstellt");
     }
+*/
+
+        static void erstelleGegner(String name, int lebenspunkte) {
+            String gegnerName = name;
+            int gegnerLeben = lebenspunkte;
+            System.out.println("Gegner wurde erstellt: " + gegnerName + " ,LP: " + gegnerLeben);
+
+        }
 
 
 }
