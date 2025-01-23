7cee9e43b9797461549825cba2787dd9c22b71fd
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-01-23 09:57:02 +0100

    + und - taschenrechner mit return

---
+++ exercises/methodenreturn.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class methodenreturn {
+
+    public static void main(String[] args) {
+        
+        int summe = taschenrechner(5, 4, '+');
+        System.out.println(summe);
+    }
+
+    public static int taschenrechner(int zahl1, int zahl2, char operator) {
+
+        int summe;
+
+        if (operator == '+'){
+            summe = zahl1 + zahl2;
+        } else {
+            summe = zahl1 - zahl2;
+        }
+
+        
+        return summe;
+
+    }
+}
