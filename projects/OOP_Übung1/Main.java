726e8d487d54ea058a1c53f46f953d02658dd156
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-02-04 08:26:10 +0100

    Übung1 OOP1

---
+++ projects/OOP_Übung1/Main.java
@@ -0,0 +1,15 @@
+package projects.OOP_Übung1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+        laptop getData = new laptop(20, 20.40, "Mark");
+        System.out.println("Ram: " + getData.ram + ", Preis: " + getData.preis + ", Marke: " + getData.marke);
+
+
+
+
+    }
+}
---
+++ projects/OOP_Übung1/laptop.java
@@ -0,0 +1,13 @@
+package projects.OOP_Übung1;
+
+public class laptop {
+    int ram;
+    double preis;
+    String marke;
+
+    public laptop(int ram, double preis, String marke){
+        this.ram = ram;
+        this.preis = preis;
+        this.marke = marke;
+    }
+}
