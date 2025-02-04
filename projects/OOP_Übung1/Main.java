b62289f31796c5318e0f36df2d92be06209fefeb
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-02-04 09:30:46 +0100

    Selfmade Aufgabe

--- projects/OOP_Übung1/Main.java
+++ projects/OOP_Übung1/Main.java
@@ -4,7 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        // Übung 2
+        /*
+         * Aufgae: 
+         * 
+         * Erstelle aus der eben erstellten Klasse 2 Objekte: 
+         * 
+         * 1. samsungLaptop: ram: 8gb, preis:600, marke: Samsung
+         * 2. medionLaptop: ram 4gb, preos: 400, marke: Medion
+         * 
+         * Und rufe die Methode getData() mit dem Objek samsungLaptop und medionLaptop auf.
+         */
+
+        laptop samsungLaptop = new laptop(8, 600, "Samsung");
+        laptop medionLaptop = new laptop(4, 400, "Medion");
+
+
+        samsungLaptop.getData();
+        medionLaptop.getData();
+
+        System.out.println("-----------------------------------------------------");
+
+        Pc dubaro = new Pc(1000, "dubaro", 32, "Intel ","RTX 5080","B450");
+        Pc selfmade = new Pc(900, "selfmade", 16, "AMD ","RTX 5080","B550");
+
+        dubaro.getPcData();
+        selfmade.getPcData();
 
     }
 }
--- projects/OOP_Übung1/laptop.java
+++ projects/OOP_Übung1/laptop.java
@@ -1,5 +1,22 @@
 package projects.OOP_Übung1;
 
+// Übung 1
+/*
+ * Aufgabe: 
+ * 
+ * Erstelle eine Klasse mit dem Namen laptop
+ * Diese Klasse soll folgende Eigenschaften haben: 
+ * 
+ * ram; 
+ * preis;
+ * marke; 
+ * 
+ * Definiere einen Konstriktor der diese 3 Eigenschaften beim Erstellen eines Objektes, dieses 
+ * Objekt direkt mit Werte befüllt.
+ * 
+ * Definiere eine Methode die diese 3 Eigenschaften beim Aufrufen der Methode in der Konsole ausgibt.
+ * 
+ */
 public class laptop {
     int ram;
     double preis;
@@ -12,6 +29,6 @@ public class laptop {
     }
 
     public void getData(){
-        System.out.println("Ram: " + this.ram + "Preis: " + this.preis + " Marke: " + this.marke);
+        System.out.println("Ram: " + this.ram + " Preis: " + this.preis + " Marke: " + this.marke);
     }
 }
