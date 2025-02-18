867d2b51e0396a6db354ce119fbbd9be538ac7b8
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-02-18 10:14:15 +0100

    Removed TODO Auto Method

--- OOP/OOP_5/Vogel.java
+++ OOP/OOP_5/Vogel.java
@@ -4,14 +4,10 @@ public class Vogel extends Tier {
 
     @Override
     public void essen() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Der Vogel isst");
     }
 
     @Override
     public void atmen() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Der Vogel atment");
     }
 
     
